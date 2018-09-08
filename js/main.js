f98642a22b146dcee0819b86cfba7a860e2da7c6
sirStephen <itobitoast@gmail.com>
2018-09-08 22:55:38 +0100

    my portfolio

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// function openSlideMenu() {
+//   document.getElementById('side-menu').style.width = '250px';
+//   document.getElementById('main').style.marginRight = '250px';  
+//   document.getElementById('main').style.backgroundColor = 'black'; 
+// }
+
+// function closeSlideMenu() {
+//   document.getElementById('side-menu').style.width = '0';
+//   document.getElementById('main').style.marginRight = '0';  
+//   document.getElementById('main').style.backgroundColor = 'black'; 
+// }
